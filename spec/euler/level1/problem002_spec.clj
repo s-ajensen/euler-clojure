a4a3cc36c54b976729f24576274f3f25d5b3402d
Alex Jensen <alex@endure.io>
2023-04-19 12:15:19 -0400

    02 - Third test passes

--- spec/euler/level1/problem002_spec.clj
+++ spec/euler/level1/problem002_spec.clj
@@ -7,10 +7,13 @@
 
 (describe "Euler Problem #2"
 
-  (it "should return vec containing 2 as first even fib number"
+  (it "should return first even fib number"
     (should= [2] (take 1 (even-fib))))
 
-  (it "should return vec containing [2 8] as first two even fib numbers"
-    (should= [2 8] (take 2 (even-fib)))))
+  (it "should return vec containing first two even fib numbers"
+    (should= [2 8] (take 2 (even-fib))))
+
+  (it "should return vec containing first three even fib numbers"
+    (should= [2 8 34] (take 3 (even-fib)))))
 
 (run-specs)
