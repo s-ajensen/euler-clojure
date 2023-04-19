753dee86cf4ad66e8b112e9edbaf18cfe2c27db7
Alex Jensen <alex@endure.io>
2023-04-19 12:51:55 -0400

    02 - Optimized even-fib

--- src/euler/level1/problem002.clj
+++ src/euler/level1/problem002.clj
@@ -1,8 +1,8 @@
 (ns euler.level1.problem002)
 
 (defn even-fib
-  ([] (even-fib 1 1))
-  ([n m] (filter even? (lazy-seq (cons n (even-fib m (+' n m)))))))
+  ([] (even-fib 2 8))
+  ([n m] (lazy-seq (cons n (even-fib m (+' n (* 4 m)))))))
 
 (defn euler-2 [n]
   (reduce + (take-while #(> n %) (even-fib))))
