8ba323545572138d0cde3431266bfeeb221f827c
Alex Jensen <alex@endure.io>
2023-04-19 12:13:14 -0400

    02 - Second test passes

--- spec/euler/level1/problem002_spec.clj
+++ spec/euler/level1/problem002_spec.clj
@@ -8,6 +8,9 @@
 (describe "Euler Problem #2"
 
   (it "should return vec containing 2 as first even fib number"
-    (should= [2] (take 1 (even-fib)))))
+    (should= [2] (take 1 (even-fib))))
+
+  (it "should return vec containing [2 8] as first two even fib numbers"
+    (should= [2 8] (take 2 (even-fib)))))
 
 (run-specs)
--- src/euler/level1/problem002.clj
+++ src/euler/level1/problem002.clj
@@ -1,4 +1,5 @@
 (ns euler.level1.problem002)
 
-(defn even-fib []
-  (lazy-seq '(2)))
+(defn even-fib
+  ([] (even-fib 1 1))
+  ([n m] (filter even? (lazy-seq (cons n (even-fib m (+' n m)))))))
