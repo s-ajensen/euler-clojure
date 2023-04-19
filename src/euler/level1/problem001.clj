323f2eb77e561584d5d847824225529cea84d0fe
Alex Jensen <alex@endure.io>
2023-04-19 10:32:16 -0400

    Finished problem 1

--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,16 @@
-(ns euler.level1.problem001)
+(ns euler.level1.problem001
+  (:require [clojure.set :as set]))
+
+(defn get-multiples [n max]
+  (loop [m          n
+         multiples  #{}]
+    (if (>= m max)
+      multiples
+      (recur (+ m n) (conj multiples m)))))
+
+(defn sum-multiples [n max]
+  (let [multiples (reduce set/union (map #(get-multiples % max) n))]
+    (reduce + multiples)))
 
 (defn euler-1 [n]
-  )
+  (sum-multiples [3 5] n))
