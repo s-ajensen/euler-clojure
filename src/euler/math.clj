e247b67d04ebe024e3bb2ba630ab40b7bd3319cd
Alex Jensen <alex@endure.io>
2023-05-01 12:36:45 -0400

    008 solved

--- src/euler/math.clj
+++ src/euler/math.clj
@@ -6,3 +6,6 @@
 (defn squares
   ([] (squares 1))
   ([n] (lazy-seq (cons (+' (square n)) (squares (inc n))))))
+
+(defn prime? [n]
+  (nil? (first (filter #(zero? (mod n %)) (range 2 (inc (Math/floor (Math/sqrt n))))))))
